dc951eb9d5060116921d7ba916a4f428eefaa93d
Natpaphon Sukitpaneenit <natpasukit@gmail.com>
2017-09-05 15:58:33 +0700

    add cron task
    - add cron task to read file stats every minute

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const app = express();
 const path = require('path');
 const formidable = require('formidable');
 const fs = require('fs');
+const cron = require('node-cron');
 
 // Static and Routing
 app.use(express.static('public')); // serving css , js for html
-app.use(express.static('uploads')); 
+app.use(express.static('uploads'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -23,13 +24,33 @@ app.post('/upload' , function(req, res){
     fs.rename(file.path, path.join(form.uploadDir, file.name));
     response += file.name+' ';
   });
-
-  // repond to client
+  // response to client
   form.on('end', function() {
    res.end(response);
   });
   form.parse(req);
 });
+
+// Reading Directory every minutes
+cron.schedule('*/1 * * * *', function(err){
+  if(!err){
+    console.log('Detect every minute');
+    fs.readdir(__dirname + '/uploads', function(err,filenames){
+      if(!err){
+        filenames.forEach(function(filename){
+          fs.stat(__dirname + '/uploads/' + filename,function(err,fstats){
+            console.log(filename);
+            console.log(fstats.ctime);
+          });
+        });
+      }else{
+        console.log(err);
+      }
+    });
+  }else{
+    console.log(err);
+  }
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
